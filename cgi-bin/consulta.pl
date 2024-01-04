97fba4866c7eaa3b6182a4992d40bddbef5f4d15
mramirezcc <mramirezcc@unsa.edu.pe>
2024-01-03 23:03:40 -0500

    implementación de lógica de comparación

--- cgi-bin/consulta.pl
+++ cgi-bin/consulta.pl
@@ -19,7 +19,20 @@ my $cgi = CGI->new;
 my $campo = $cgi->param('campo');
 my $valor = $cgi->param('valor');
 
-
+my $aux;
+if(!($valor eq "")){
+  open(IN,"../data/ProgramasDeUniversidades.csv") or die "<h1>Error al abrir el archivo.</h1>\n";
+  while(my $line = <IN>){
+    my %dict = matchLine($line);
+    my $result = $dict{$kind};
+    if(defined($result) && $result =~ /.*$keyword.*/){
+      print "<h1>Encontrado: $line</h1>\n";
+      $aux = 1;
+      next;
+    }
+  }
+  close(IN);
+}
 print <<BLOCK;
 </body>
 </html>
