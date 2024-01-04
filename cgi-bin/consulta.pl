13f468121284caf9e05046ec4ef3df02f0f5825a
mramirezcc <mramirezcc@unsa.edu.pe>
2024-01-03 23:29:12 -0500

    implementacion de expresion regular para la comparación

--- cgi-bin/consulta.pl
+++ cgi-bin/consulta.pl
@@ -24,7 +24,7 @@ if(!($valor eq "")){
   open(IN,"../data/ProgramasDeUniversidades.csv") or die "<h1>Error al abrir el archivo.</h1>\n";
   while(my $line = <IN>){
     my %dict = matchLine($line);
-    my $result = $dict{$kind};
+    my $result = $dict{$valor};
     if(defined($result) && $result =~ /.*$keyword.*/){
       print "<h1>Encontrado: $line</h1>\n";
       $aux = 1;
@@ -37,3 +37,17 @@ print <<BLOCK;
 </body>
 </html>
 BLOCK
+
+sub matchLine{
+  my %dict = ();
+  my $line = $_[0];
+  if($line =~ /^[0-9]+\|(.+)\|.+\|.+\|([0-9]+)\|.+\|.+\|.+\|.+\|.+\|(.+)\|.+\|.+\|.+\|.+\|.+\|(.+).+/){
+    $dict{"Nombre"} = $1;
+    $dict{"Periodo_de_Licenciamiento"} = $2;
+    $dict{"Departamento_Local"} = $3;
+    $dict{"Denominacion_Programa"} = $4;
+  }else{
+    print "<h1>Error, datos incorrectos en la línea: $line</h1>\n";
+  }
+  return %dict;
+}
